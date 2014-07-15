4e2937e708598e2ee5719fb1c27e75cad1b9806a
Julian Adams <joolsa@gmail.com>
2014-07-15 21:58:19 +0100

    update 4.0 -> 5.0 to work with Xamarin Studio 5.0

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins
-rm ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/unity-mimetypes.dll
-cp -p bin/Debug/unity-mimetypes.dll ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/
-ls -l ~/Library/Application\ Support/XamarinStudio-4.0/LocalInstall/Addins/
+mkdir -p ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins
+rm ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/unity-mimetypes.dll
+cp -p bin/Debug/unity-mimetypes.dll ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/
+ls -l ~/Library/Application\ Support/XamarinStudio-5.0/LocalInstall/Addins/
